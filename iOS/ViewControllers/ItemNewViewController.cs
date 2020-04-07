552eab4531ee70444f171f67665df7e24de5de8e
Cosmin Stirbu <cosmin.stirbu@fortech.ro>
2020-04-07 18:07:36 +0300

    CLLocationManager memory example

--- iOS/ViewControllers/ItemNewViewController.cs
+++ iOS/ViewControllers/ItemNewViewController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-
+using System.Linq;
+using CoreLocation;
+using Foundation;
+using MapKit;
 using UIKit;
 
 namespace MemoryDemo.iOS
 {
-    public partial class ItemNewViewController : UIViewController
+    public partial class ItemNewViewController : UIViewController, IMKMapViewDelegate
     {
+        private CLLocationManager _locationManager = new CLLocationManager();
+
         public ItemsViewModel ViewModel { get; set; }
 
         public ItemNewViewController(IntPtr handle) : base(handle)
@@ -16,32 +21,24 @@ namespace MemoryDemo.iOS
         {
             base.ViewDidLoad();
 
-            // The lambda above causes a memory leak
+            // The view controller leaks because of TouchUpInside (as expected) but if you comment it
+            // it doesn't leak for LocationsUpdated and UpdatedHeading (when it actually should leak)
 
-            btnSaveItem.TouchUpInside += (sender, e) => {
-                var item = new Item
-                {
-                    Text = txtTitle.Text,
-                    Description = txtDesc.Text
-                };
-                ViewModel.AddItemCommand.Execute(item);
-                NavigationController.PopToRootViewController(true);
-            };
-        }
+            // btnSaveItem.TouchUpInside += SaveButtonPressed;
 
-        /*
-        public override void ViewWillAppear(bool animated)
-        {
-            base.ViewWillAppear(animated);
-            btnSaveItem.TouchUpInside += SaveButtonPressed;
-        }
+            _locationManager.RequestWhenInUseAuthorization();
+            _locationManager.DesiredAccuracy = CLLocation.AccurracyBestForNavigation;
 
-        public override void ViewWillDisappear(bool animated)
-        {
-            base.ViewWillDisappear(animated);
-            btnSaveItem.TouchUpInside -= SaveButtonPressed;
+            _locationManager.LocationsUpdated += LocationsUpdated;
+            _locationManager.UpdatedHeading += HeadingChanged;
+
+            _locationManager.StartUpdatingLocation();
+            _locationManager.StartUpdatingHeading();
+
+            // If you set the delegate from C# then in causes a circular reference, but if you set it from the storyboard, it doesn't.
+            // I guess that's expected since the delegate was set only in Obj-C runtime
+            // MapView.Delegate = this;
         }
-        */
 
         void SaveButtonPressed(object sender, EventArgs e)
         {
@@ -52,6 +49,22 @@ namespace MemoryDemo.iOS
             };
             ViewModel.AddItemCommand.Execute(item);
             NavigationController.PopToRootViewController(true);
+        }
+
+        [Export("mapView:regionDidChangeAnimated:")]
+        public void RegionChanged(MKMapView mapView, bool animated)
+        {
+            txtDesc.Text = $"MapRegion : {mapView.VisibleMapRect}";
+        }
+
+        private void HeadingChanged(object sender, CLHeadingUpdatedEventArgs e)
+        {
+            txtTitle.Text = $"Heading : {e.NewHeading}";
+        }
+
+        private void LocationsUpdated(object sender, CLLocationsUpdatedEventArgs e)
+        {
+            txtDesc.Text = $"Location : {e.Locations.First().Coordinate}";
         }
     }
 }
